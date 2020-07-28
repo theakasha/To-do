60ad4201863814fc10459d03443a97ae46e160bf
theakasha <akash23aryan@gmail.com>
2020-07-28 12:39:49 +0530

    server created

--- node-examples/index.js
+++ node-examples/index.js
@@ -2,14 +2,15 @@ var rect=require('./rectangle');
 
 function solveRect(l,b){
     console.log("Solving the rectangle with l = "+l+"and b "+b);
-
-    if(l<=0||b<=0){
-        console.log("Rectangle dimensions invalid");
-    }
-    else{
-        console.log("The area of the rectanglse is "+rect.area(l,b));
-        console.log("The perimeter is "+ rect.perimeter(l,b));
-    }
+    rect(l,b,(err,rectangle)=>{
+        if(err){
+            console.log("ERROR:",err.message);
+        }else {
+            console.log("The area of the rectangle of dimensions l= " + l + "and b = " + b + "is" + rectangle.area());
+            console.log("The perimeter of the rectangle of dimensions l= " + l + "and b = " + b + "is" + rectangle.perimeter());
+        }
+    });
+    console.log("This statement is after the call to rect()");
 }
 
 solveRect(2,4);
