06a0f7c665752e2464ee59155a3005a328cfaa2f
theakasha <akash23aryan@gmail.com>
2020-07-28 09:29:54 +0530

    Simple Node Modules

--- node-examples/index.js
+++ node-examples/index.js
@@ -1,17 +1,14 @@
-var rect={
-    perimeter:(x,y)=>(2*(x+y)),
-    area:(x,y)=>(x*y)
-};
+var rect=require('./rectangle');
 
 function solveRect(l,b){
-    console.log("Solving the rectangle with l="+l+"and b"+b);
+    console.log("Solving the rectangle with l = "+l+"and b "+b);
 
     if(l<=0||b<=0){
         console.log("Rectangle dimensions invalid");
     }
     else{
-        console.log("The area of the rectanglse is"+rect.area(l,b));
-        console.log("The perimeter is"+ rect.perimeter(l,b));
+        console.log("The area of the rectanglse is "+rect.area(l,b));
+        console.log("The perimeter is "+ rect.perimeter(l,b));
     }
 }
 
